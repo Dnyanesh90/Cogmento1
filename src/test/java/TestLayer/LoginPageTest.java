7d86274ab9db8dce4a9c4c7018ac7f3a0e7fec91
Dnyaneshwar <kadamdnyana@gmail.com>
2023-03-13 16:09:40 +0530

    Cogmento1

---
+++ src/test/java/TestLayer/LoginPageTest.java
@@ -0,0 +1,30 @@
+package TestLayer;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
+
+@Test(groups= {"LoginPage","Regression"})
+public class LoginPageTest extends BaseClass{
+
+	@BeforeMethod
+	@BeforeTest
+	public void setUp()
+	{
+		BaseClass.inilization();
+	}
+	
+	@Test
+	public void validateLoginFunctionality()
+	{
+		LoginPage loginpage =new LoginPage();
+		loginpage.loginFunctionality("dnyaneshkadam0702@gmail.com", "7798913075");
+		//loginpage.loginFunctionality(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	
+	
+}
